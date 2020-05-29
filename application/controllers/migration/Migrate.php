7dd6966fe133df54114c8c5fe2d80536ab13ac1c
irfanjoelax <irfanthejoelax@gmail.com>
2020-05-29 11:33:45 +0800

    v.1.4 - config dan migration

---
+++ application/controllers/migration/Migrate.php
@@ -0,0 +1,51 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migrate extends CI_Controller
+{
+
+   /**
+    * function construct untuk dijalankan pertama kali
+    */
+   public function __construct()
+   {
+      parent::__construct();
+
+      // load library yang digunakan
+      $this->load->library('migration');
+   }
+
+   /**
+    * function untuk migration sesuai dengan version pada url browser
+    */
+   public function version($ver)
+   {
+      if ($this->migration->version($ver) === FALSE) {
+         show_error($this->migration->error_string());
+      } else {
+         echo 'Migration Successfully For Sequential Version : ' . $ver;
+      }
+   }
+
+   /**
+    * function untuk membuat dummy data dengan seeder
+    */
+   public function seeder($table)
+   {
+      // data untuk user
+      // $data = array();
+
+      // $this->db->insert($table, $data);
+      // echo 'Seeder Table ' . $table . ' is Successfully';
+   }
+
+   // function untuk menghapus atau mengkosong table 
+   public function truncate($table)
+   {
+      $this->db->truncate($table);
+      echo 'Truncate Table ' . $table . ' is Successfully';
+   }
+}
+
+/* End of file Migrate.php */
---
+++ application/migrations/001_create_fakultas.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_create_fakultas extends CI_Migration
+{
+   public function up()
+   {
+      $this->dbforge->add_field(array(
+         'id_fklt' => array(
+            'type' => 'INT',
+            'constraint' => 5,
+            'unsigned' => TRUE
+         ),
+         'nama_fklt' => array(
+            'type' => 'VARCHAR',
+            'constraint' => '100',
+         ),
+      ));
+      $this->dbforge->add_key('id_fklt', TRUE);
+      $this->dbforge->create_table('fakultas');
+   }
+
+   public function down()
+   {
+      $this->dbforge->drop_table('fakultas');
+   }
+}
+
+/* End of file create_fakultas.php */
